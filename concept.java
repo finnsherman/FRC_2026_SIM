4978479ce43f678fa37dd6f2bebc442e2dd3b970
math-rad <ben.sir545@outlook.com>
2026-01-24 14:13:53 -0600

    me & finn work on java sim

---
+++ concept.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class concept {
+
+    private record InterpInput (double distance, double v_x, double v_y, double v_z) { };
+    private record InterpOutput (double yaw, double pitch) { }; 
+
+    double maxVelComponent = 6.7;
+    double simIncrement = 0.02;
+    double minDistance = 1;
+    double maxDistance = 5;
+    double targetHeight = 2;
+    
+    int distanceEntries = Double.valueOf(Math.floor((maxDistance - minDistance) / simIncrement)).intValue();
+    int velocityComponenEntries = Double.valueOf(Math.floor((2 * maxVelComponent) / simIncrement)).intValue();
+
+    Map<InterpInput, InterpOutput> lookupTable = new HashMap<>();
+
+    public concept() {
+        for (double velX = -maxVelComponent; velX <= maxVelComponent; velX += simIncrement ) {
+            for (double velY = -maxVelComponent; velY <= maxVelComponent; velY += simIncrement ) {
+                for (double velZ = -maxVelComponent; velZ <= maxVelComponent; velZ += simIncrement) {
+                    if (Math.sqrt(Math.pow(velX, 2) + Math.pow(velY, 2) + Math.pow(velZ, 2)) <maxVelComponent) {
+                        for (double distance = minDistance; distance <= maxDistance; distance += simIncrement) {
+                            lookupTable.put(
+                                new InterpInput(distance, velX, velY, velZ),
+                                new InterpOutput(
+                                    Math.atan2(distance - ),
+                                    Math.atan2()
+                                ) 
+                            )
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
