8c11378cf2db79c6403c05fffafe15ee4911fe6c
math-rad <ben.sir545@outlook.com>
2026-01-27 14:54:25 -0600

    add more constants + iter variables

--- Revision/Constants.java
+++ Revision/Constants.java
@@ -4,6 +4,14 @@ public class Constants {
     public static final double turretHeight = 0.5588; // height at which game piece leaves turret
     public static final double gravitationalConstant = 9.81; // gravity
     public static final double targetHeight = 2; // target height
+
+
+    // TBT
+    public static final double targetX = 6;
+    public static final double targetY = 7;
+
+    public static final double fieldMinX = 0;
+    public static final double fieldMinY = 0;
     public static final double fieldMaxX = 10;
-    public static final double fieldMaxY = 10; // to change these to actual values later
+    public static final double fieldMaxY = 10;
 }
--- Revision/Interpolator.java
+++ Revision/Interpolator.java
@@ -1,15 +1,35 @@
 package Revision;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+import static java.lang.Math.atan2;
+
 import java.lang.invoke.ConstantBootstraps;
 
 public class Interpolator {
     double graivty = Constants.gravitationalConstant;
-    double targetHeight = Constants.targetHeight;
     double increment = 0.2;
     double maxVelocityComponent = 6.7;
-    double maxPX = Constants.fieldMaxX;
-    double maxPY = Constants.fieldMaxY;
+
+    double TX = Constants.targetX;
+    double TY = Constants.targetY;
+    double TZ = Constants.targetHeight;
 
     // At most all you should know to input is how fast the ROBOT is moving and where it is. 
-    private record ShotInput(double px, double py, double vx, double vy);
+    private record ShotInput(double px, double py, double vx, double vy) { };
+    private record ShotCanidate(ShotInput input, double pitch, double yaw) { };
+
+    public Interpolator() {
+        for (double px = Constants.fieldMinX; px <= Constants.fieldMaxX; px += increment) {
+            for (double py = Constants.fieldMinY; py <= Constants.fieldMaxY; py += increment) {
+                double dx = TX - px;
+                double dy = TY - py;
+                double r = sqrt(pow(dx, 2) + pow(dy, 2));
+                double toTargetYaw = atan2(dy, dx);
+                double toTargetPitch = atan2(TZ, r);
+
+            }
+        }
+    }
 
 }
